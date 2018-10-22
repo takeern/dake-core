224d27d533ef2782d21a8dd74759905c3ecc9c07
tangqi <tangqi@bilibili.com>
2018-10-22 11:33:35 +0800

    [INIT]

--- distTs/component/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function parseConfig(config) {
-    config.entryScript += config.isTs ? '.js' : '.ts';
-    let { port } = config;
-}
-exports.default = parseConfig;
-//# sourceMappingURL=main.js.map
