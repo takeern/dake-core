3f47cf6c7c3df86229169417cc584564d0c964c9
tangqi <tangqi@bilibili.com>
2018-12-06 17:20:11 +0800

    [UPDATE]

--- distTs/configFactory/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./component/main");
-exports.default = main_1.default;
--- distTs/main.js
+++
@@ -1,6 +0,0 @@
-const parseConfig = require('./configFactory/index').default;
-const ssrRender = require('./ssrRender/index').default;
-module.exports = {
-    parseConfig,
-    ssrRender,
-};
